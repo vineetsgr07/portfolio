620049b5ffa434937aba9c554ab954e22a1e7286
vineet <vineet.sgr07@gmail.com>
2019-11-02 22:37:49 +0530

    added article 2

--- src/routes/posts/2019-01-06-welcome /post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Tutorial for blog`,
-  tags: ['Javascript'],
-  spoiler: "A brief discription and explanation of the rendering mechaine of famous javascript frameworks",
-  getContent: () => import('./document.mdx'),
-}
