7dd984d51711d90d364772fe02dc8579cec1fd46
vineet <vineet.sgr07@gmail.com>
2019-11-18 03:07:52 +0530

    spelling mistake correction

--- src/routes/posts/2019-10-01-React-vs-Angular/post.js
+++ src/routes/posts/2019-10-01-React-vs-Angular/post.js
@@ -1,8 +1,8 @@
 export default {
   title: `Architecture overview: React and AngularJs`,
   tags: ['Javascript'],
-  spoiler: `Mechanism which fuel react and angular to boost performance, Working of React's
-  virtual dom and it's diffing algorithm. How change detection work in Angular 
+  spoiler: `The mechanism which fuel react and angular to boost performance, Working of React's
+  virtual dom and it's diffing algorithm. How to change detection work in Angular 
   and ViewData created by it for comparison of different component`,
   getContent: () => import('./document.mdx'),
 }
--- src/routes/posts/2019-11-05-Microservice-Explained/post.js
+++ src/routes/posts/2019-11-05-Microservice-Explained/post.js
@@ -1,8 +1,9 @@
 export default {
   title: `Microservice Architecture Explained`,
   tags: ['Javascript'],
-  spoiler: `When to Choose Microservices over Monolithic. Introduction to message broker and it's 
-  variety,  Load balancer and algorithms used behind it, Why Domain Driven Development prior 
-  development and Sagas to maintain concurrency in microservice architecture`,
+  spoiler: `When to Choose Microservices over Monolithic. Introduction to message broker 
+  and it's variety, Load balancer and algorithms used behind it, Why Domain Driven 
+  Development prior development and Sagas to maintain concurrency in a microservice 
+  architecture`,
   getContent: () => import('./document.mdx'),
 }
