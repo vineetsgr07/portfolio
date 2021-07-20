a4cb6d5f26ca10226128e17b4ccab1c74b09520d
Vineet <vineet.sgr07@gmail.com>
2021-07-20 14:12:18 +0530

    added build

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -16,7 +16,8 @@ function Bio(props) {
       <img src={photoURL} alt="Me" />
       <p>
         {/* Personal Blog by <a href="http://localhost:3000/about">Vineet</a> */}
-        Personal Blog by <a href="http://www.vineetsagar.space/about">Vineet</a>
+        {/* Personal Blog by <a href="http://www.vineetsagar.space/about">Vineet</a> */}
+        Personal Blog by <a href="http://vineet-portfolio.s3-website.us-east-2.amazonaws.com//about">Vineet</a>
         <div>Connecting the gap between technologies... </div>
       </p>
     </div>
